b8c9d79898de804de91079f7f1f70f732138a7c1
Gotvna <nvogt@gaming.tech>
2022-01-17 21:09:02 +0100

    css

--- main.php
+++ main.php
@@ -4,9 +4,33 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
+    <link rel="stylesheet" href="css/style.css">
     <title>Association Katochi</title>
 </head>
 <body>
-    
+
+    <div id="header">
+        <div class="background_header">
+
+        </div>
+        <div class="logo">
+
+        </div>
+        <div class="description">
+            <div class="title">
+                <h1>Katochi</h1>
+            </div>
+            <div class="content">
+
+            </div>
+        </div>
+    </div>
+
+
+
+
+<script type="text/javascript" src="js/materialize.js"></script>
 </body>
 </html>
