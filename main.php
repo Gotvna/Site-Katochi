baabf3a27c00290859c4d3eee96c065d34ca8a14
Gotvna <nvogt@gaming.tech>
2022-01-17 21:53:21 +0100

    add img

--- main.php
+++ main.php
@@ -6,25 +6,22 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link type="text/css" rel="stylesheet" href="css/materialize.css"  media="screen,projection"/>
-    <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
     <title>Association Katochi</title>
 </head>
 <body>
 
-    <div id="header">
-        <div class="background_header">
-
-        </div>
-        <div class="logo">
-
+    <div class="header">
+        <div class="logo center">
+            <img src="img/circle.jpg">
         </div>
         <div class="description">
             <div class="title">
                 <h1 class="center">Katochi</h1>
             </div>
-            <div class="content">
-                <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque illo id aperiam quae officiis magni iste quas inventore. At, nam fugiat obcaecati eius, molestiae quos aperiam consequatur totam nemo sequi laboriosam recusandae ipsam enim? Ad ea cum, laudantium dicta velit commodi quod doloremque totam harum, blanditiis dolorum modi! Quos, ex. Commodi aliquid debitis repellat est amet omnis, distinctio voluptas dolor. Quis dolor et voluptate odio sint. Praesentium illo quibusdam, voluptatum reprehenderit tempora omnis exercitationem! Quasi distinctio laboriosam provident expedita sunt dicta officiis dolores, sequi cumque tempore, nemo ipsa. Delectus quos sint ipsa explicabo praesentium? Quae ea consequuntur, ipsum distinctio molestiae magnam libero illum quidem similique nisi doloribus quasi deserunt. Expedita, iste iure! At ducimus magni mollitia, maxime veniam, repellendus culpa autem molestias totam fugiat corporis nam quidem a ex odio ratione molestiae repudiandae aspernatur voluptates, ullam praesentium quibusdam. Consequatur officiis possimus cupiditate sequi odit magnam incidunt tempora odio minus iste vitae, est velit ducimus! Ut harum minus mollitia inventore deserunt iusto culpa aut non minima nesciunt rem, quis tempore magni, eveniet tempora quae ex porro. Dicta necessitatibus, facilis blanditiis error vel rerum quibusdam reiciendis et, odio aspernatur delectus ab laudantium sequi, facere itaque unde eligendi officiis quis perferendis exercitationem suscipit. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, doloribus nesciunt modi tempora officiis tempore natus porro? Minima voluptatem eum debitis odit quis voluptates tenetur reprehenderit! In dicta iusto vitae enim, ab amet eius et ex aspernatur adipisci, distinctio cumque modi molestias sit, autem nulla corrupti officiis exercitationem. Deleniti dolores veritatis perferendis laboriosam rem officiis eligendi alias quae cum possimus at repudiandae, suscipit, a similique mollitia beatae aspernatur. Porro error qui expedita rem quis distinctio soluta fugiat nisi ipsa. Expedita quis dolore molestiae odio? Tenetur molestiae quae repellat. Eaque aliquam officiis esse animi suscipit! Expedita, vitae. Nihil culpa quasi, quia hic sint autem, numquam maxime reprehenderit totam quos aliquam exercitationem vero provident nostrum placeat expedita odit, assumenda alias beatae quod in dignissimos. Maiores fugiat recusandae, earum consectetur autem placeat eos sint quibusdam alias et optio vitae ipsa reprehenderit minima ex ullam non cupiditate consequuntur ad eveniet quas. Aut placeat architecto natus, voluptatum temporibus exercitationem, magnam explicabo eveniet quaerat ratione quas unde repudiandae perferendis quos facilis omnis possimus dolorum ullam in sunt molestias voluptatibus odio dicta debitis! Quos iure pariatur excepturi libero labore alias quasi, totam ipsa, mollitia praesentium assumenda perspiciatis, atque dolorum temporibus facere natus consectetur delectus autem quisquam quod!</p>
+            <div class="content center">
+                <p> Lorem ipsum dolor sit amet ita quis  Expedita, vitae. Nihil culpa quasi, quia hic sint autem, numquam maxime reprehenderit totam quos aliquam exercitationem vero provident nostrum placeat expedita odit, assumenda alias beatae quod in dignissimos. Maiores fugiat recusandae, earum consectetur autem placeat eos sint quibusdam alias et optio vitae ipsa reprehenderit minima ex ullam non cupiditate consequuntur ad eveniet quas. Aut placeat architecto natus, voluptatum temporibus exercitationem, magnam explicabo eveniet quaerat ratione quas unde repudiandae perferendis quos facilis omnis possimus dolorum ullam in sunt molestias voluptatibus odio dicta debitis! Quos iure pariatur excepturi libero labore alias quasi, totam ipsa, mollitia praesentium assumenda perspiciatis, atque dolorum temporibus facere natus consectetur delectus autem quisquam quod!</p>
             </div>
         </div>
     </div>
